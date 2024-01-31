cb8e4ef0d6305ebe6efce751a845aee930a9ea9c
Felix <Felixgustavodonel@gmail.com>
2024-01-31 11:03:36 -0300

    mensagem de sucesso!

--- Orbita.Frontend/src/view/CreateStudent.vue
+++ Orbita.Frontend/src/view/CreateStudent.vue
@@ -1,67 +1,78 @@
 <template>
-    <div>
-      <NavegationbarComponent title="Alunos"/>
-      <v-form @submit.prevent="salvarAluno">
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="aluno.nome" label="Nome" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="aluno.email" label="Email" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="aluno.cpf" label="CPF" required></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-btn type="submit" color="primary">Salvar</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </div>
-  </template>
-  
-  <script>
-  import StudentService from '@/services/StudentService';
-  import NavegationbarComponent from '../components/NavegationbarComponent.vue';
-  
-  export default {
-    data() {
-      return {
-        aluno: {
-          nome: '',
-          email: '',
-          cpf: ''
-        }
-      };
-    },
-    components: {
-      NavegationbarComponent,
-    },
-    methods: {
-      async salvarAluno() {
-        try {
-  
-          const response = await StudentService.createstudent({
-            ra: 0,
-            name: this.aluno.nome,
-            email: this.aluno.email,
-            cpf: this.aluno.cpf,
-          });
-          this.limparCampos();
-        } catch (error) {
-          console.error('Erro na solicitação:', error);
-        }
+  <div>
+    <NavegationbarComponent title="Alunos"/>
+    <v-form @submit.prevent="salvarAluno">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="aluno.nome" label="Nome" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="aluno.email" label="Email" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="aluno.cpf" label="CPF" required  @input="filterNumeric"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row v-if="showSuccessMessage">
+          <v-col>
+            <v-alert type="success" dismissible @input="showSuccessMessage = false">
+              Aluno cadastrado com sucesso!
+            </v-alert>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn type="submit" color="primary">Salvar</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import StudentService from '@/services/StudentService';
+import NavegationbarComponent from '../components/NavegationbarComponent.vue';
+
+export default {
+  data() {
+    return {
+      aluno: {
+        nome: '',
+        email: '',
+        cpf: '',
       },
-      limparCampos() {
-        this.aluno.nome = '';
-        this.aluno.email = '';
-        this.aluno.cpf = '';
-      },
-    }
-  };
-  </script>
-  
+      showSuccessMessage: false, 
+    };
+  },
+  components: {
+    NavegationbarComponent,
+  },
+  methods: {
+    async salvarAluno() {
+      try {
+        const response = await StudentService.createstudent({
+          ra: 0,
+          name: this.aluno.nome,
+          email: this.aluno.email,
+          cpf: this.aluno.cpf,
+        });
+
+        // Exibe a mensagem de sucesso
+        this.showSuccessMessage = true;
+
+        // Limpa os campos
+        this.limparCampos();
+      } catch (error) {
+        console.error('Erro na solicitação:', error);
+      }
+    },
+    limparCampos() {
+      this.aluno.nome = '';
+      this.aluno.email = '';
+      this.aluno.cpf = '';
+    },
+  },
+};
+</script>
--- Orbita.Frontend/src/view/EditStudent.vue
+++ Orbita.Frontend/src/view/EditStudent.vue
@@ -1,75 +1,84 @@
 <template>
-    <div>
-      <NavegationbarComponent title="Alunos"/>
-      <v-form @submit.prevent="Poststudent">
-        <v-container>
+  <div>
+    <NavegationbarComponent title="Alunos" />
+    <v-form @submit.prevent="Poststudent">
+      <v-container>
+        <v-col cols="12" md="12">
+          <v-text-field v-model="student.ra" label="RA" :readonly="true" required></v-text-field>
+        </v-col>
+        <v-row>
           <v-col cols="12" md="12">
-            <v-text-field v-model="student.ra" label="RA" :readonly="true" required></v-text-field>
+            <v-text-field v-model="student.name" label="Nome" required></v-text-field>
           </v-col>
-          <v-row>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="student.name" label="Nome" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="student.email" label="Email" required></v-text-field>
-            </v-col>
-            <v-col cols="12" md="12">
-              <v-text-field v-model="student.cpf" label="CPF" :readonly="true" required></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-btn type="submit" color="primary">Salvar</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </div>
-  </template>
-  
-  <script>
-  import StudentService from '@/services/StudentService';
-  import NavegationbarComponent from '../components/NavegationbarComponent.vue';
-  
-  export default {
-    data() {
-      return {
-        student: {
-          ra: '',
-          name: '',
-          email: '',
-          cpf: '',
-        },
-      };
-    },
-    components: {
-      NavegationbarComponent,
-    },
-    created() {
-      this.getStudent(); // Chama o método getStudent para preencher os dados
-    },
-    methods: {
-      async getStudent() {
-        try {
-          const id = this.$route.params.id;
-          this.student = await StudentService.getStudent(id);
-        } catch (error) {
-          console.error('Erro ao buscar Aluno:', error);
-        }
+          <v-col cols="12" md="12">
+            <v-text-field v-model="student.email" label="Email" required></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field v-model="student.cpf" label="CPF" :readonly="true" required></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row v-if="showSuccessMessage">
+          <v-col>
+            <v-alert type="success" dismissible @input="showSuccessMessage = false">
+              Aluno cadastrado com sucesso!
+            </v-alert>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn type="submit" color="primary">Salvar</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import StudentService from '@/services/StudentService';
+import NavegationbarComponent from '../components/NavegationbarComponent.vue';
+
+export default {
+  data() {
+    return {
+      student: {
+        ra: '',
+        name: '',
+        email: '',
+        cpf: '',
       },
-      async Poststudent() {
-        try {
-          const response = await StudentService.Poststudent({
-            ra: this.student.ra,
-            name: this.student.name,
-            email: this.student.email,
-            cpf: this.student.cpf,
-          });
-        } catch (error) {
-          console.error('Erro na solicitação:', error);
-        }
-      },
+      showSuccessMessage: false
+    };
+  },
+  components: {
+    NavegationbarComponent,
+  },
+  created() {
+    this.getStudent();
+  },
+  methods: {
+    async getStudent() {
+      try {
+        const id = this.$route.params.id;
+        this.student = await StudentService.getStudent(id);
+      } catch (error) {
+        console.error('Erro ao buscar Aluno:', error);
+      }
     },
-  };
-  </script>
-  
+    async Poststudent() {
+      try {
+        const response = await StudentService.Poststudent({
+          ra: this.student.ra,
+          name: this.student.name,
+          email: this.student.email,
+          cpf: this.student.cpf,
+        });
+
+        this.showSuccessMessage = true;
+      } catch (error) {
+        console.error('Erro na solicitação:', error);
+      }
+    },
+  },
+};
+</script>
