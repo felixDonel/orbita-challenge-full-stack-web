d3083ce3fa01ed3a57c407197b041f94baf5afe6
Felix <Felixgustavodonel@gmail.com>
2024-01-31 23:43:07 -0300

    Validações!

--- Orbita.Frontend/src/view/CreateStudent.vue
+++ Orbita.Frontend/src/view/CreateStudent.vue
@@ -5,13 +5,17 @@
       <v-container>
         <v-row>
           <v-col cols="12" md="12">
-            <v-text-field v-model="aluno.nome" label="Nome" required></v-text-field>
+            <v-text-field v-model="aluno.name" label="Nome"  :rules="nameRules" required></v-text-field>
+            <v-messages :value="aluno.name" :error-messages="nameErrors"></v-messages>
+          </v-col>
+
+          <v-col cols="12" md="12">
+            <v-text-field v-model="aluno.email" label="Email" :rules="emailRules" required></v-text-field>
+            <v-messages :value="aluno.email" :error-messages="emailErrors"></v-messages>
           </v-col>
           <v-col cols="12" md="12">
-            <v-text-field v-model="aluno.email" label="Email" required></v-text-field>
-          </v-col>
-          <v-col cols="12" md="12">
-            <v-text-field v-model="aluno.cpf" label="CPF" required  @input="filterNumeric"></v-text-field>
+            <v-text-field v-model="aluno.cpf" label="CPF" :rules="cpfRules" required></v-text-field>
+            <v-messages :value="aluno.cpf" :error-messages="cpfErrors"></v-messages>
           </v-col>
         </v-row>
         <v-row v-if="showSuccessMessage">
@@ -21,6 +25,7 @@
             </v-alert>
           </v-col>
         </v-row>
+
         <v-row>
           <v-col>
             <v-btn type="submit" color="primary">Salvar</v-btn>
@@ -34,42 +39,64 @@
 <script>
 import StudentService from '@/services/StudentService';
 import NavegationbarComponent from '../components/NavegationbarComponent.vue';
-
 export default {
   data() {
     return {
       aluno: {
-        nome: '',
+        name: '',
         email: '',
         cpf: '',
       },
       showSuccessMessage: false, 
+      nameErrors: [],
+      emailErrors: [],
+      cpfErrors:[],
     };
   },
   components: {
     NavegationbarComponent,
+  }
+  ,
+  computed: {
+    nameRules() {
+      return [
+        value => !!value || 'Nome é obrigatório.',
+        value => (value && value.length >= 3) || 'Nome deve ter pelo menos 3 caracteres.',
+      ];
+    },
+    emailRules() {
+      return [
+        value => !!value || 'E-mail é obrigatório.',
+        value => (/.+@.+\..+/.test(value)) || 'E-mail deve ser válido.',
+      ];
+    },
+    cpfRules() {
+      return [
+        value => !!value || 'CPF é obrigatório.',
+        value => (value && value.length === 11) || 'CPF deve ter 11 caracteres.',
+      ];
+    },
   },
   methods: {
     async salvarAluno() {
       try {
+        console.log("teste");
         const response = await StudentService.createstudent({
           ra: 0,
-          name: this.aluno.nome,
+          name: this.aluno.name,
           email: this.aluno.email,
           cpf: this.aluno.cpf,
         });
 
-        // Exibe a mensagem de sucesso
         this.showSuccessMessage = true;
 
-        // Limpa os campos
         this.limparCampos();
       } catch (error) {
         console.error('Erro na solicitação:', error);
       }
     },
     limparCampos() {
-      this.aluno.nome = '';
+      this.aluno.name = '';
       this.aluno.email = '';
       this.aluno.cpf = '';
     },
--- Orbita.Frontend/src/view/EditStudent.vue
+++ Orbita.Frontend/src/view/EditStudent.vue
@@ -8,13 +8,16 @@
         </v-col>
         <v-row>
           <v-col cols="12" md="12">
-            <v-text-field v-model="student.name" label="Nome" required></v-text-field>
+            <v-text-field v-model="student.name" label="Nome"  :rules="nameRules" required></v-text-field>
+            <v-messages :value="student.name" :error-messages="nameErrors"></v-messages>
           </v-col>
           <v-col cols="12" md="12">
             <v-text-field v-model="student.email" label="Email" required></v-text-field>
+            <v-messages :value="student.email" :error-messages="emailErrors"></v-messages>
           </v-col>
           <v-col cols="12" md="12">
-            <v-text-field v-model="student.cpf" label="CPF" :readonly="true" required></v-text-field>
+            <v-text-field v-model="student.cpf" label="CPF" :rules="cpfRules" required></v-text-field>
+            <v-messages :value="student.cpf" :error-messages="cpfErrors"></v-messages>
           </v-col>
         </v-row>
         <v-row v-if="showSuccessMessage">
@@ -47,7 +50,10 @@ export default {
         email: '',
         cpf: '',
       },
-      showSuccessMessage: false
+      showSuccessMessage: false, 
+      nameErrors: [],
+      emailErrors: [],
+      cpfErrors:[],
     };
   },
   components: {
@@ -56,6 +62,27 @@ export default {
   created() {
     this.getStudent();
   },
+  computed: {
+    nameRules() {
+      return [
+        value => !!value || 'Nome é obrigatório.',
+        value => (value && value.length >= 3) || 'Nome deve ter pelo menos 3 caracteres.',
+      ];
+    },
+    emailRules() {
+      return [
+        value => !!value || 'E-mail é obrigatório.',
+        value => (/.+@.+\..+/.test(value)) || 'E-mail deve ser válido.',
+      ];
+    },
+    cpfRules() {
+      return [
+        value => !!value || 'CPF é obrigatório.',
+        value => (value && value.length === 11) || 'CPF deve ter 11 caracteres.',
+      ];
+    },
+  },
+
   methods: {
     async getStudent() {
       try {
